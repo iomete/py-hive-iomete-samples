9ea9c025da07be08f0baa7897c1f07a8c58381ff
Fuad Musayev <46884462+fmusayev@users.noreply.github.com>
2023-02-14 05:28:55 +0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,11 +1,11 @@
 # default: None, example: frankfurt.iomete.com
-HOST = "cgaiqk.iomete.cloud"
-WORKSPACE_ID = "sa4j3-7a9"            # default: None, example: 910848238944
+HOST = "{cluster_id}.iomete.cloud"
+WORKSPACE_ID = "abcde-123"            # default: None, example: 910848238944
 LAKEHOUSE = "analytics"  # default: None, example: reporting
 DATABASE = "default"                 # default: "default", example: db1
 # default: none, example: john or john@company.com
-USERNAME = "fuad@iomete.com"
+USERNAME = ""
 # default: none, example: my_strong_pass
-PASSWORD = "9QlfYl43tGHx3M2v5UDsGbfn/M1yk+DRUSRiF0kUT0Q="
+PASSWORD = ""
 
 TABLE = "employees"                     # table to query
